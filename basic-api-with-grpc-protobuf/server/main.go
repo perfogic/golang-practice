e20ae93f5270d56281156201fdb37679b537c871
hello <longdieu12x@gmail.com>
2023-11-05 22:51:56 +0700

    learn sample api with grpc, protobuf

---
+++ basic-api-with-grpc-protobuf/server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	proto "basic-api-with-grpc-protobuf/proto-go/service"
+	"context"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		panic(err)
+	}
+	srv := grpc.NewServer()
+
+	proto.RegisterAddServiceServer(srv, &server{})
+	reflection.Register(srv)
+
+	if e := srv.Serve(listener); e != nil {
+		panic(e)
+	}
+}
+
+type server struct {
+	proto.UnimplementedAddServiceServer
+}
+
+func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+
+	result := a + b
+
+	return &proto.Response{Result: result}, nil
+}
+
+func (s *server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+
+	result := a * b
+
+	return &proto.Response{Result: result}, nil
+}
